ce506bc1f7e57f52e8cb36a4fa2e90e55de32c37
Eric Naumburger <79154603+laal3@users.noreply.github.com>
2022-02-11 13:58:59 +0100

    Added functions

--- bot/music.py
+++ bot/music.py
@@ -1,10 +1,12 @@
+import re
+
 class Music(bot):
     def __init__(self, bot):
         self.bot = bot
         self.queue = []
         self.shuffle = false
 
-    def play():
+    def play(url:str):
         pass
 
     def pause():
@@ -29,11 +31,31 @@ class Music(bot):
         pass
 
     def drop_queue():
+        self.queue = []
+
+    def remove_from_queue(remove_param):
+        if remove_param.startswith('http'):
+            pass
+        elif isinstance(remove_param, int):
+            pass
+        elif remove_param == re("[0-9]+-[0-9]"):
+            remove_range = remove_param.split("-")
+
+            if(remove_range[0] > remove[1]):
+                return false
+
+            for i in range(remove_range[0], remove_range[1]):
+               self.queue.pop(i)
+
+
+
+    def connect():
         pass
 
-    def remove_from_queue():
+    def disconnect():
         pass
 
 
 class Player():
-    pass
+    def play(url:str):
+        pass
