8e7ce2c5856b84b156aedd77291eedeccfa5a857
laal3 <ericnaumi56@gmail.com>
2022-02-17 20:32:08 +0100

    Getting docker to work properly

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 FROM python:3.10-buster
 
-WORKDIR /usr/app/src
+RUN mkdir /app
+ADD . /app
+WORKDIR /app
 
 COPY bot/requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
+RUN pip3 install python-dotenv
 
 COPY . .
 
-ENTRYPOINT [ "python", "main.py"]
+ENTRYPOINT [ "python", "bot/main.py"]
--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,14 @@
 from discord.ext import commands
+import discord
 import os
 from dotenv import load_dotenv
+load_dotenv()
 from function import write_message
 
 bot = commands.Bot(command_prefix='~')
-load_dotenv()
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+print("." + CLIENT_SECRET + ".")
 
 @bot.event
 async def on_ready():
@@ -15,12 +18,15 @@ async def on_ready():
 async def on_message(message):
   if message.author == bot.user:
     return
+  if discord.ChannelType.private:
+    return
+  
 
 #repeats your message in your choosen text channel
 #~write <channel name> <message>
 @bot.command()
 async def write(context, *args):
-  write_message(context, *args)
+  await write_message(context, *args)
   
 
 @bot.command()
